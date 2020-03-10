01fb594e0c54f7b6e824a818bbe6ce3f688f83c8
Khosro <Jadidi>
2020-03-10 22:12:17 +0100

    added javascript for buttons
    Added basic javascript code for buttons.
    Please nitice that function include no logic! these must be added later.
    tested in chrome->inspect->console.
    no errors so far.

---
+++ script.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', () => {
+    let newlistButton = document.getElementById('new_list_button');
+    newlistButton.onclick = loadNewList;
+
+    function loadNewList() {
+        //load a new list here
+    };
+    let addBookButton = document.getElementById('add_book-button');
+    addBookButton.onclick = addBook;
+
+    function addBook() {
+        //add book here
+    };
+    let filterBooks = document.getElementById('filter_button');
+    filterBooks.onclick = filterBooksInList;
+
+    function filterBooksInList() {
+        //filter books here
+    };
+    let changeBook = document.getElementById('change_book_button');
+    changeBook.onclick = changeSelectedBook;
+
+    function changeSelectedBook() {
+        //change book here
+    };
+    let removeBook = document.getElementById('remove_book_button');
+    removeBook.onclick = removeSelectedBook;
+
+    function removeSelectedBook() {
+        //remove selected book here
+    };
+});
