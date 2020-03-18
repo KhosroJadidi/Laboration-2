836bc3119629aaae1e9bb6ae8a3b709a217cbbe8
Khosro <Jadidi>
2020-03-18 23:12:38 +0100

    [work in progress] added a view data function
    Line 35-67
    added a view function which:
    - gets a new key
    -saves it
    -requests access to API 10 times
    -informs the user about success or failure.
    -itterates over data array

--- script.js
+++ script.js
@@ -1,75 +1,106 @@
-window.addEventListener('load', () => {
-
+window.addEventListener("load", () => {
     const apiURL = "https://www.forverkliga.se/JavaScript/api/crud.php";
     var apiKey = null;
-    
-    function removeClass(id,classToRemove){
-        let element=document.getElementById(id);
+    var viewDataRequestStatus = null;
+    var viewDataRecievedArray=null;
+
+    function removeClass(id, classToRemove) {
+        let element = document.getElementById(id);
         element.classList.remove(classToRemove);
-    };
+    }
 
-    function addClass(id,classToAdd){
-        let element=document.getElementById(id);
+    function addClass(id, classToAdd) {
+        let element = document.getElementById(id);
         element.classList.add(classToAdd);
-    };
+    }
 
-    function setInnerHTML(id,HTML_Content){
-        let element= document.getElementById(id);
-        element.innerHTML=HTML_Content;
-    };
+    function setInnerHTML(id, HTML_Content) {
+        let element = document.getElementById(id);
+        element.innerHTML = HTML_Content;
+    }
 
-    let newlistButton = document.getElementById('new_list_button');
+    let newlistButton = document.getElementById("new_list_button");
     newlistButton.onclick = loadNewList;
 
-    function loadNewList() {
-        fetch(apiURL + '?requestKey')
-        .then(function(response) {
-            return response.json(); 
-        })
-        .then(function(data) {
-            apiKey = data.key;
-        })
-        .catch(function(message) {
-            console.log(message)
-        });
-        console.log(apiKey);
-    };
+    function fetchNewKey() {
+        fetch(apiURL + "?requestKey")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => (apiKey = data.key))
+            .catch(message => console.log(message));
+        console.log(`Key is : ${apiKey}`);
+    }
 
-    let addBookButton = document.getElementById('add_book-button_id');
+    function loadNewList() {
+        fetchNewKey();
+        if (apiKey) {
+            for (let index = 0; index <= 10; index++) {
+                fetch(`${apiURL}?op=select&key=${apiKey}`)
+                    .then(response => {
+                        return response.json()
+                    })
+                    .then(json => {
+                        viewDataRequestStatus = json.status;
+                        viewDataRecievedArray=json.data;
+                    })
+                    .catch(message => console.log(message));
+
+                if (viewDataRequestStatus === 'success') {
+                    console.log(`status after ${index+1} try(s): ${viewDataRequestStatus}`);
+                    console.log('Successfully connected to API.');
+                    console.log(viewDataRecievedArray);
+                    //varför ingen print? kanske array length =0???
+                    for (const iterator of viewDataRecievedArray) {
+                        console.log(`Författare: ${iterator.author}, Title: ${iterator.title}`)
+                    }
+                    break;
+                }
+                if (index >= 10) {
+                    console.log(`status after ${index} try(s): ${viewDataRequestStatus}`);
+                    console.log(`We failed to contact tha API after ${index} tries. Please try again in a few seconds.`)
+                }
+            }
+        } else {
+            console.log('"loadNewList" does not have a key to work with. Try again.');
+        };
+    }
+
+    let addBookButton = document.getElementById("add_book-button_id");
     addBookButton.onclick = addBook;
 
     function addBook() {
         //add book here
-        removeClass('input_fields_id', 'invisible');
-        removeClass('add_confirm_section', 'invisible')
-    };
-    
-    let filterBooks = document.getElementById('filter_button_id');
+        removeClass("input_fields_id", "invisible");
+        removeClass("add_confirm_section", "invisible");
+    }
+
+    let filterBooks = document.getElementById("filter_button_id");
     filterBooks.onclick = filterBooksInList;
 
     function filterBooksInList() {
         //filter books here
-        removeClass('author_and_title_input_fields_id', 'invisible');
-    };
-    let changeBook = document.getElementById('change_book_button');
+        removeClass("author_and_title_input_fields_id", "invisible");
+    }
+    let changeBook = document.getElementById("change_book_button");
     changeBook.onclick = changeSelectedBook;
 
     function changeSelectedBook() {
         //change book here
-    };
-    let removeBook = document.getElementById('remove_book_button');
+    }
+    let removeBook = document.getElementById("remove_book_button");
     removeBook.onclick = removeSelectedBook;
 
     function removeSelectedBook() {
         //remove selected book here
-    };
+    }
 
-    let resetInputFields = document.getElementById('filter_reset_button');
-    resetInputFields.onclick=resetEnteredText;
-    
-    function resetEnteredText(){
-        let authorField= document.getElementById('author_id');
-        let titleField= document.getElementById('title_id');
-        authorField.value=titleField.value="";
-    };
+    let resetInputFields = document.getElementById("filter_reset_button");
+    resetInputFields.onclick = resetEnteredText;
+
+    function resetEnteredText() {
+        let authorField = document.getElementById("author_id");
+        let titleField = document.getElementById("title_id");
+        authorField.value = titleField.value = "";
+    }
 });
